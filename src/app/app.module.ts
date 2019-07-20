cbe9a4d9eaf0fb9215f0ab90decd65833606892d
Honeysalad <oliveira.douglasedu@outlook.com>
2019-07-20 00:31:32 -0300

    Added Bootstrap, created CreateComponent and it's template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
