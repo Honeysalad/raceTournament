18ce497951d7aeb135d9511fb09b4f30001cabbb
Honeysalad <oliveira.douglasedu@outlook.com>
2019-07-21 15:48:16 -0300

    Implemented forms in CreateComponent and stylized hover and disabled buttons

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Tournament } from '../shared/tournament.model';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
+  public tournament: Tournament;
+
+  public form: FormGroup = new FormGroup({
+    'name': new FormControl(null, [Validators.required]),
+    'races': new FormControl(null, [Validators.required]),
+    'score': new FormControl(null, [Validators.required]),
+    'team1entrant': new FormControl(null, [Validators.required]),
+    'team1name': new FormControl(null, [Validators.required]),
+    'team1country': new FormControl(null, [Validators.required]),
+    'team1pilot1name': new FormControl(null, [Validators.required]),
+    'team1pilot1country': new FormControl(null, [Validators.required]),
+    'team1pilot1number': new FormControl(null, [Validators.required])
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public createTournament(): void {
+    console.log(this.form.controls);
   }
 
 }
