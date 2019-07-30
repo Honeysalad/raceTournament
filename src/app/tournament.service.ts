bf080c16636906cb1ec86c16d7d863ca9f19bae0
Honeysalad <oliveira.douglasedu@outlook.com>
2019-07-30 16:06:17 -0300

    added a matrix to save race results

--- src/app/tournament.service.ts
+++ src/app/tournament.service.ts
@@ -3,6 +3,7 @@ import { TOURNAMENT } from './tournament';
 export class TournamentService {
 
     private tournament: Tournament = TOURNAMENT;
+    private races: number[][] = [];
 
     public getTournament(): Tournament {
         return this.tournament;
