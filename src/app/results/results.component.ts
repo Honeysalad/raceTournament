49d7207fbf7a99b82571efc460385fe4a622b22f
Honeysalad <oliveira.douglasedu@outlook.com>
2019-07-31 15:22:31 -0300

    added point assign and binding

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TournamentService } from '../tournament.service';
+import { Tournament } from '../shared/tournament.model';
+import { Router } from '@angular/router';
+import { Driver } from '../shared/driver.model';
 
 @Component({
   selector: 'app-results',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
-  constructor() { }
+  tournament: Tournament;
+  race: number[];
+  grid = [];
+
+  constructor(
+    private tournamentService: TournamentService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.tournament = this.tournamentService.getTournament();
+    this.race = this.tournamentService.getLastRace();
+
+    for (let [i, number] of this.race.entries()) {
+      for (let team of this.tournament.getTeams()) {
+        for (let driver of team.getDrivers()) {
+          if (driver.getNo() === number) {
+            this.grid.push([driver.getName(), team.getName(), driver.getPoints()[driver.getPoints().length - 1]]);
+          }
+        }
+      }
+    }
   }
 
 }
