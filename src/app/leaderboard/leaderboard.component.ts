2dd237f56e6851afc2fb27b2324e7b539a67a219
Honeysalad <oliveira.douglasedu@outlook.com>
2019-07-31 17:16:25 -0300

    implemented binding and logic to leaderboard

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Driver } from '../shared/driver.model';
+import { Team } from '../shared/team.model';
+import { Tournament } from '../shared/tournament.model';
+import { TournamentService } from '../tournament.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-leaderboard',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaderboardComponent implements OnInit {
 
-  constructor() { }
+  private driversLeaderboard = [];
+  private teamsLeaderboard: Team[] = [];
+
+  private tournament: Tournament;
+
+  constructor(
+    private tournamentService: TournamentService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    // tslint:disable: prefer-const
+    this.tournament = this.tournamentService.getTournament();
+    for (let team of this.tournament.getTeams()) {
+      for (let driver of team.getDrivers()) {
+        this.driversLeaderboard.push([driver.getName(), team.getName(), driver.sumPoints()]);
+      }
+      this.teamsLeaderboard.push(team);
+    }
+    this.driversLeaderboard.sort((a, b) => {
+      return a[2] > b[2] ? -1 : a[2] < b[2] ? 1 : 0;
+    });
+    this.teamsLeaderboard.sort((a, b) => {
+      return a.sumPoints() > b.sumPoints() ? -1 : a.sumPoints() < b.sumPoints() ? 1 : 0;
+    });
   }
 
 }
